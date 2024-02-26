ffe09a94a0f65c6f057e0a207acc6c66ed53075c
eagle218 <113504886+eagle218@users.noreply.github.com>
2024-02-27 00:35:20 +0200

    Update app.py

--- app.py
+++ app.py
@@ -19,7 +19,7 @@ app.config["SESSION_PERMANENT"] = False
 app.config["SESSION_TYPE"] = "filesystem"
 Session(app)
 
-# Configure CS50 Library to use SQLite database
+
 db = SQL("sqlite:///finance.db")
 
 # Make sure API key is set
